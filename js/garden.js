f83127c63fbf5e32094cd777cbcd9812c8705e5d
Jerke Combee <jerke1988@gmail.com>
2025-01-07 23:35:46 +0100

    Add spawning mecanix for cats

--- js/garden.js
+++ js/garden.js
@@ -16,9 +16,34 @@ template.innerHTML = `
       text-align: center;
       background-color: var(--color-primary-500);
     }
+    .sub-header {
+      font-size: 1.5em;
+      padding-top: 16px;
+      padding-bottom: 16px;
+      padding-left: 28px;
+      padding-right: 28px;
+      gap: 16px;
+    }
+    .cat {
+      width: 128px;
+      height: 128px;
+      border-radius: 12px;
+      overflow: hidden;
+      border: solid 3px var(--color-primary-700);
+    }
+    #visitors {
+      display: flex;
+      flex-wrap: wrap;
+      padding-top: 16px;
+      padding-bottom: 16px;
+      padding-left: 28px;
+      padding-right: 28px;
+    }
   </style>
   <div class="wrapper">
     <div class="header">Garden</div>
+    <div class="sub-header">Visitors</div>
+    <div id="visitors"></div>
   <div>
 `;
 
@@ -27,6 +52,24 @@ class Garden extends HTMLElement {
     super();
     const shadow = this.attachShadow({ mode: "open" });
     shadow.appendChild(template.content.cloneNode(true));
+    this.visitors = shadow.getElementById("visitors");
+  }
+
+  connectedCallback() {
+    setInterval(() => {
+      const interesting = window.context.state.interesting;
+      if (Math.floor(Math.random() * 100) < interesting + 1) {
+        this.spawnCat();
+      }
+    }, 1000);
+  }
+
+  spawnCat() {
+    const catId = Math.floor(Math.random() * 4);
+    const cat = document.createElement("img");
+    cat.src = files.images["cat-" + catId];
+    cat.classList.add("cat");
+    this.visitors.appendChild(cat);
   }
 }
 
